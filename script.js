80615814af8505a22bf4f2cdefb38d55f1826787
Ashish <chakravartiashish2406@gmail.com>
2023-06-02 00:14:06 +0530

    Final work saving and ready to push

---
+++ script.js
@@ -0,0 +1,29 @@
+let text = document.querySelector(".text");
+let i = document.querySelector("i");
+let nav = document.querySelector(".nav")
+let logo= document.querySelector(".logo")
+let navImg= document.querySelector(".nav-img")
+
+let flag = 0;
+text.addEventListener("click", () => {
+    if (flag == 0) {
+        nav.style.top = 0;
+        navImg.style.left = 0;
+        text.innerHTML = '<i class="ri-close-line"></i>'
+        logo.style.color="black"
+        text.style.color="black"
+
+        flag = 1;
+
+    }
+    else {
+        nav.style.top= '-700px';
+        text.innerHTML = '<i class="ri-menu-line"></i>'
+        logo.style.color="white"
+        text.style.color="white"
+
+
+        flag = 0;
+
+    }
+})
