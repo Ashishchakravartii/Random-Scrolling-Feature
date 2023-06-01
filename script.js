725330c0e509e481ae18df1c02efb495baa44765
Ashish <chakravartiashish2406@gmail.com>
2023-06-02 00:50:56 +0530

    Made responsive

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
 let text = document.querySelector(".text");
 let i = document.querySelector("i");
 let nav = document.querySelector(".nav")
-let logo= document.querySelector(".logo")
-let navImg= document.querySelector(".nav-img")
+let logo = document.querySelector(".logo")
 
 let flag = 0;
 text.addEventListener("click", () => {
     if (flag == 0) {
         nav.style.top = 0;
-        navImg.style.left = 0;
         text.innerHTML = '<i class="ri-close-line"></i>'
-        logo.style.color="black"
-        text.style.color="black"
+        logo.style.color = "black"
+        text.style.color = "black"
 
         flag = 1;
 
     }
     else {
-        nav.style.top= '-700px';
+        nav.style.top = '-1000px';
         text.innerHTML = '<i class="ri-menu-line"></i>'
-        logo.style.color="white"
-        text.style.color="white"
-
-
+        logo.style.color = "white"
+        text.style.color = "white"
         flag = 0;
 
     }
